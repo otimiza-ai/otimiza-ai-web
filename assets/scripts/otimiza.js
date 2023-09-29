488a1435b857f01d7db5fed9523a08abbb8f5167
Fernando Ott <ferott@gmail.com>
2023-09-29 15:09:41 -0300

    Website v2

---
+++ assets/scripts/otimiza.js
@@ -0,0 +1,15 @@
+
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log('oi');
+    $("#close-mobile-menu").click(function(){
+        console.log('hide');
+        $("#headlessui-portal-root").hide();
+    });
+
+    $("#open-mobile-menu").click(function(){
+        console.log('show');
+        $("#headlessui-portal-root").show();
+    });
+});
